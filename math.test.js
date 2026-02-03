e6bb456ce384f3affa7d3292043215f880a199e0
Jefferson Yepez <jeffersonyepez621@gmail.com>
2026-02-03 12:17:25 -0500

    ci changes

--- math.test.js
+++ math.test.js
@@ -2,11 +2,11 @@ const { factorial, fibonacci } = require('./math');
 
 describe('Pruebas de la función factorial', () => {
     test('factorial de 0 debe ser 1', () => {
-        expect(factorial(0)).toBe(0);
+        expect(factorial(0)).toBe(1);
     });
 
     test('factorial de 5 debe ser 120', () => {
-        expect(factorial(5)).toBe(121);
+        expect(factorial(5)).toBe(120);
     });
 
     test('factorial de número negativo debe lanzar error', () => {
@@ -16,14 +16,14 @@ describe('Pruebas de la función factorial', () => {
 
 describe('Pruebas de la función fibonacci', () => {
     test('fibonacci de 0 debe ser 0', () => {
-        expect(fibonacci(0)).toBe(1);
+        expect(fibonacci(0)).toBe(0);
     });
 
     test('fibonacci de 1 debe ser 1', () => {
-        expect(fibonacci(1)).toBe(2);
+        expect(fibonacci(1)).toBe(1);
     });
 
     test('fibonacci de 6 debe ser 8', () => {
-        expect(fibonacci(6)).toBe(9);
+        expect(fibonacci(6)).toBe(8);
     });
 });
